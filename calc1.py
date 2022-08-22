cb93483606b0fe59153c40e7785476a87f04004d
Sergio <sgomezja@cisco.com>
2022-08-22 17:05:19 -0500

    rev3

--- calc1.py
+++ calc1.py
@@ -16,6 +16,8 @@ def divide (a,b):
 	return a / b 
 
 
+
+
 print("Thank you for using the calculator script")
 print("")
 print("Please choose your option")
@@ -33,17 +35,19 @@ while True:
 	#This will check depending of user input between 1-4
 	if choice in ("1", "2", "3", "4"):
 		n1= float (input("Enter the 1st number: "))
-		n2= float (input("Enter the 2nd number"))
+		n2= float (input("Enter the 2nd number: "))
 
 		if choice == "1":
-			print (n1, "+", n2, "=" , addition(n1, n2))
+			print ("\t\t\t\t",n1, "+", n2, "=" , addition(n1, n2))
 
 		elif choice == "2":
-				print (n1, "-", n2, "=", subctraction(n1,n2))
+				print ("\t\t\t\t",n1, "-", n2, "=", subctraction(n1,n2))
 		elif choice == "3":
-				print (n1, "*", n2, "=", multiply(n1,n2))
+				print ("\t\t\t\t",n1, "*", n2, "=", multiply(n1,n2))
 		elif choice == "4":
-				print (n1, "/", n2, "=", divide(n1,n2))
+				print ("\t\t\t\t",n1, "/", n2, "=", divide(n1,n2))
+
+
 
 		break
 
